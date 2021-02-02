949538dc3b38183245c36e825e26078f2a1eeae7
Ezhilnilavan Nagarajan <ezhilnilavan13@gmail.com>
2021-02-01 23:35:07 -0500

    Add files via upload

---
+++ src/redux/configs.js
@@ -0,0 +1,7 @@
+
+export const CAGE_API_PATH = `https://api.opencagedata.com/geocode/v1/json?key=APIKEY`;
+
+export const OPEN_WEATHER_API_PATH = 'https://api.openweathermap.org/data/2.5/forecast?appid=APIKEY&units=imperial';
+
+export const OPEN_WEATHER_ICON_PATH = 'https://openweathermap.org/img/wn/';
+
